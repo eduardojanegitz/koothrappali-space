e1033795394cd4949e54f033e5b6ee5d38a16246
Eduardo Alves de Lima Filho <877@vitafor.local>
2023-03-17 13:18:48 -0300

    adding photo filter

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from "../../components/Header";
-import Menu from "../../components/Menu";
 import styles from "./HomePage.module.scss";
-// import banner from "./banner.png"
-import Banner from "../../components/Banner";
-import Footer from "../../components/Footer";
-import Gallery from "../../components/Gallery";
-import Popular from "../../components/Popular";
+import Header from "components/Header";
+import Menu from "components/Menu";
+import Banner from "components/Banner";
+// import Footer from "components/Footer";
+import Gallery from "components/Gallery";
+import Popular from "components/Popular";
 
 export default function HomePage() {
     return (
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Tags from "../Tags";
 import styles from "./Gallery.module.scss";
 import photos from "./photos.json";
+import { useState } from "react";
 
 import Cards from "./Cards";
 
 export default function Gallery() {
+
+    const [itens, setItens] = useState(photos);
+    const tags = [...new Set(photos.map(value => value.tag))]
+
+    const filterPhotos = (tag) => {
+        const newPhotos = photos.filter(photo => {
+            return photo.tag === tag;
+        })
+        setItens(newPhotos);
+    }
     return (
         <section className={styles.gallery}>
             <h2>Navegue pela Galeria</h2>
-            <Tags />
-            <Cards photos={photos} styles={styles}/>
+            <Tags tags={tags} filterPhotos={filterPhotos} setItens={setItens}/>
+            <Cards photos={itens} styles={styles}/>
         </section>
     )
 }
